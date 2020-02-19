a06b188317812b8c195826fa296a411ac58b23f2
Andrey <andrey.sviontek@gmail.com>
2020-02-19 22:23:41 +0500

    feat: typical arrays problems is solved

--- src/index.js
+++ src/index.js
@@ -1,12 +1,21 @@
 
 exports.min = function min (array) {
-  return 0;
+    if (array === undefined || array.length === 0) {
+        return 0;
+    }
+  return array.sort((a,b) => (a-b)).splice(0, 1);
 }
 
 exports.max = function max (array) {
-  return 0;
+    if (array === undefined || array.length === 0) {
+        return 0;
+    }
+    return array.sort((a,b) => (a-b)).splice(-1, 1);
 }
 
 exports.avg = function avg (array) {
-  return 0;
+    if (array === undefined || array.length === 0) {
+        return 0;
+    }
+  return array.reduce((a, b) => a+b) / array.length;
 }
